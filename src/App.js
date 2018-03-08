4656e9dfaed421a0c03b285a329dd90e5b3cc7d0
C4Q <c4q@C4Qs-MBP-80.home>
2018-03-07 21:34:55 -0500

    resume added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to my Website2</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href="./umed_resume.pdf">resume<a/>
       </div>
     );
   }
